6f245fe88e20810b09721b64b943d7b0705f7373
IAML00X <cronoluke@gmail.com>
2016-11-29 13:42:32 +0100

    Fixed

--- src/xamp/GUI/MainViewController.java
+++ src/xamp/GUI/MainViewController.java
@@ -7,8 +7,6 @@ package xamp.GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -17,7 +15,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.MediaPlayer;
-
+import xamp.XAmp;
 
 /**
  * FXML Controller class
@@ -52,32 +50,21 @@ public class MainViewController implements Initializable {
     private ImageView playButton;
     @FXML
     private Slider volumeSlider;
-    
-    private MediaPlayer mp;
-    
 
+    private MediaPlayer mp;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 
     @FXML
-    private void initialize(MouseEvent event) 
-    {
-       
+    private void initialize(MouseEvent event) {
+        XAmp.playSong();
     }
 
     /*
     * setting the value for the volumeSlider. 
     * the value is 1 so therefor we have to *100
-    */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) 
-    {
-        volumeSlider.setValue(mp.getVolume() * 100);
-        volumeSlider.valueProperty().addListener(new InvalidationListener() {
-            @Override
-            public void invalidated(Observable observable) {
-                    mp.setVolume(volumeSlider.getValue()/100);//here we convert the value and therefor devide it by 100
-            }
-        });
-    
-    }
-    
+     */
 }
