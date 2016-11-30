486666135e00839577a0e2f7571e265a35f06a65
IAML00X <cronoluke@gmail.com>
2016-11-30 10:16:26 +0100

    Moved stuff around

--- src/xamp/BLL/PlaylistManager.java
+++ src/xamp/BLL/PlaylistManager.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package xamp.DAL;
+package xamp.BLL;
 
 /**
  *
  * @author IAMLUX
  */
-public class DALManager {
+public class PlaylistManager {
 
 }
